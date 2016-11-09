ddf0497cfa4f21af733cd9ba140837242bb4e5c4
angussidney <angussidney@users.noreply.github.com>
2016-11-09 14:20:02 +1100

    Models

--- shoppingapp/models.py
+++ shoppingapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    cost = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
