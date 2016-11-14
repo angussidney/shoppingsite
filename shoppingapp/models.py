29b92b98da93fe250ab48996cc114306359b00c3
angussidney <angussidney@users.noreply.github.com>
2016-11-14 15:10:49 +1100

    Remap to root

--- shoppingapp/models.py
+++ shoppingapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
     cost = models.IntegerField(default=0)
     rating = models.IntegerField(default=0)
+    stock = models.IntegerField(default=10)
--- shoppingsite/urls.py
+++ shoppingsite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^shoppingapp/', include('shoppingapp.urls')),
+    url(r'^', include('shoppingapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
