6c677a9e6675c107067cfd1606a015ca11dce706
angussidney <angussidney@users.noreply.github.com>
2016-11-18 10:36:20 +1100

    Thumbnails

--- shoppingapp/views.py
+++ shoppingapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'shoppingapp/nav.html')
+    return render(request, 'shoppingapp/home.html')
