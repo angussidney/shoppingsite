92e87faf646c503aee5797f01cfd22dc37a54908
angussidney <angussidney@users.noreply.github.com>
2016-11-17 13:06:13 +1100

    Setup base nav partially

--- shoppingapp/models.py
+++ shoppingapp/models.py
@@ -5,7 +5,8 @@ class Category(models.Model):
     category_name = models.CharField(max_length=200)
 
 class Product(models.Model):
+    category = models.ForeignKey(Category)
     product_name = models.CharField(max_length=200)
-    cost = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
     rating = models.IntegerField(default=0)
     stock = models.IntegerField(default=10)
