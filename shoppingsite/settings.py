2cf7a087b587b903f97f58e0b7c2f1b7dd6a1396
angussidney <angussidney@users.noreply.github.com>
2016-11-17 23:09:08 +1100

    Add app to installed apps and started active nav

--- shoppingsite/settings.py
+++ shoppingsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shoppingapp.apps.ShoppingappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -66,6 +67,10 @@ TEMPLATES = [
         },
     },
 ]
+
+#TEMPLATE_CONTEXT_PROCESSORS = (
+#    'django.core.context_processors.request',
+#)
 
 WSGI_APPLICATION = 'shoppingsite.wsgi.application'
 
